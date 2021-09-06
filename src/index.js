858d1431c6bce542ac6712efcd9d93e6d879a7c5
JMEChristian Digital <jmechristian@gmail.com>
2021-09-06 16:42:04 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import Swiper from 'swiper/swiper-bundle';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+  },
+});
